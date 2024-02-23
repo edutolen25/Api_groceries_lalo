fc4eb4032e0144b49523bfd0592de1a8e0f049d0
edutolen25 <eduardotolen25@gmail.com>
2024-02-23 15:59:40 -0600

    primer commit

---
+++ src/routes/products.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import {getAll, getOne, insertOne, updateOne, deleteOne} from '../controllers/products.controllers.js';
+const router = Router();
+router.get('/', getAll);
+router.get('/:barcode',getOne);
+router.post('/',insertOne);
+
+router.post("/:barcode", updateOne)
+router.get("/delete/:barcode", deleteOne)
+export default router;
